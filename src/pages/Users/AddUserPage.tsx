a7f24ba1c46f651f08f1f39027d02b11433eca37
sega-sk <mitkin93@gmail.com>
2025-07-14 00:00:08 +0300

    changes

--- src/pages/Users/AddUserPage.tsx
+++ src/pages/Users/AddUserPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import { ArrowLeft } from 'lucide-react';
 import Button from '../../components/UI/Button';
 import FormField from '../../components/Forms/FormField';
